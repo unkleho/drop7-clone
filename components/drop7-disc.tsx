ba1b4e787ad3a1726977cc3648107afa2f28f178
Kaho Cheung <kaho@elefanttraks.com>
2023-01-08 23:36:13 +1100

    Cracked disc mask test

--- components/drop7-disc.tsx
+++ components/drop7-disc.tsx
@@ -1,4 +1,5 @@
 import { motion, Transition, Variant, Variants } from 'framer-motion';
+import { url } from 'inspector';
 import React, { useRef } from 'react';
 import { DiscValue } from '../shared/drop7';
 
@@ -70,7 +71,7 @@ export const Drop7Disc: React.FC<Props> = ({
       type: 'tween',
       duration: 0.7,
       delay: index * 0.02,
-      ease: flashEaseOut,
+      ease: customEaseOutBounce,
     };
   }
 
@@ -129,9 +130,10 @@ export const Drop7Disc: React.FC<Props> = ({
       style={{
         gridRow: row + 1,
         gridColumn: column + 1,
-        backgroundColor: colour.bg,
+        // backgroundColor: colour.bg,
         willChange: 'transform',
         fontSize: 'min(6vw, 1.8rem)',
+        // clipPath: 'url(#disc-cracked)',
       }}
     >
       <span className="disc-value-shadow col-start-1 row-start-1 -mt-1 opacity-60">
@@ -157,29 +159,55 @@ export const Drop7Disc: React.FC<Props> = ({
 };
 
 const DiscCracked = () => {
+  const colour = colourMap['blank'];
+
   return (
-    <svg viewBox="0 0 100 100">
-      {[...new Array(10)].map((_, i) => {
-        const angle = 360 / 10;
-        const rotate = i * angle;
-        const pad = 14;
-        return (
-          <path
-            d={`M 100 50
-          A 50 50 0 0 0 ${getCoordFromDegrees(angle - pad, 50, 100)}
-          L ${getCoordFromDegrees(angle - pad, 30, 100)}
-          A 30 30 0 0 1 80 50`}
-            key={rotate}
-            transform={`rotate(${rotate - pad / 2})`}
-            style={{
-              transformOrigin: 'center',
-            }}
-            className={'fill-gray-700'}
-          />
-        );
-      })}
-      <circle r={22} cx="50" cy="50" className="fill-gray-700" />;
-    </svg>
+    <>
+      <svg viewBox="0 0 100 100" className="col-start-1 row-start-1">
+        <clipPath id="disc-cracked" clipPathUnits="objectBoundingBox">
+          {[...new Array(2)].map((_, i) => {
+            const angle = 360 / 10;
+            const rotate = i * angle;
+            const pad = 14;
+            console.log(getCoordFromDegrees(angle - pad, 0.5, 1));
+
+            return (
+              <path
+                d={`M 1 0.50
+            A 0.50 0.50 0 0 0 ${getCoordFromDegrees(angle - pad, 0.5, 1).join(
+              ' '
+            )}
+            L ${getCoordFromDegrees(angle - pad, 0.3, 1).join(' ')}
+            A 0.30 0.30 0 0 1 0.80 0.50`}
+                key={rotate}
+                transform={`rotate(0)`}
+                // transform={`rotate(${rotate - pad / 2})`}
+                style={{
+                  transformOrigin: 'center',
+                }}
+                className={'fill-gray-700'}
+              />
+            );
+          })}
+
+          <circle r={0.22} cx="0.5" cy="0.5" />
+          {/* <circle r={22} cx="50" cy="50" className={'fill-gray-700'} /> */}
+        </clipPath>
+
+        {/* <circle
+        r={50}
+        cx="50"
+        cy="50"
+        className="bg-gradient-to-bl from-slate-600 to-slate-900"
+        // className={[colour.bg].join(' ')}
+        clipPath="url(#disc-cracked)"
+      /> */}
+      </svg>
+      <div
+        className={['aspect-square w-full', colour.bg].join(' ')}
+        style={{ clipPath: 'url(#disc-cracked)' }}
+      ></div>
+    </>
   );
 };
 
@@ -191,52 +219,6 @@ function getCoordFromDegrees(angle: number, radius: number, svgSize: number) {
   const coordY = y * -radius + svgSize / 2;
   return [coordX, coordY];
 }
-
-type Disc3DProps = {
-  children: React.ReactNode;
-};
-
-const Disc3D: React.FC<Disc3DProps> = ({ children }) => {
-  return (
-    <div className="relative aspect-square w-full overflow-auto rounded-full">
-      <div
-        className="absolute h-full w-full"
-        style={{
-          // TODO: try rgba(var(--base-colour), 0.1) where base-colour is 0, 0, 0
-          background: `radial-gradient(at 80% 20%,#FFF478,#FFB02E,#F70A8D)`,
-        }}
-      ></div>
-      <div
-        className="absolute h-full w-full"
-        style={{
-          background: `radial-gradient(#FFB849, rgba(255, 184, 71, 0))`,
-        }}
-      ></div>
-      <div
-        className="absolute h-full w-full"
-        style={{
-          background: `radial-gradient(at 55% 40%, rgba(245, 150, 57, 0) 60%, rgba(255, 125, 206, 1))`,
-        }}
-      ></div>
-      <div
-        className="absolute h-full w-full"
-        style={{
-          background: `radial-gradient(at 54% 40%, rgba(0, 0, 0, 0) 65%, rgba(0, 0, 0, 1))`,
-          opacity: 0.6,
-        }}
-      ></div>
-      <div
-        className="absolute h-full w-full"
-        style={{
-          background: `radial-gradient(at 53% 45%, rgba(179, 35, 35, 0) 50%, rgba(0, 0, 0, 0.4))`,
-          opacity: 1,
-        }}
-      ></div>
-
-      <div className="relative h-full w-full">{children}</div>
-    </div>
-  );
-};
 
 function easeOutBounce(x: number): number {
   const n1 = 7.0625;
@@ -254,13 +236,7 @@ function easeOutBounce(x: number): number {
 }
 
 // https://stackoverflow.com/questions/740308/how-do-i-modify-this-easing-function-to-bounce-less
-function flashEaseOut(t: number, b = 0, c = 1, d = 1) {
-  // if ((t /= d) < 1 / 2.75) return c * (7.5625 * t * t) + b;
-  // else if (t < 2 / 2.75) return c * (7.5625 * (t -= 1.5 / 2.75) * t + 0.75) + b;
-  // else if (t < 2.5 / 2.75)
-  //   return c * (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375) + b;
-  // else return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
-
+function customEaseOutBounce(t: number, b = 0, c = 1, d = 1) {
   t /= d;
   if (t < 1.4 / 2.75) {
     return 3.858419 * t * t;
@@ -274,57 +250,3 @@ function flashEaseOut(t: number, b = 0, c = 1, d = 1) {
   t -= 2.625 / 2.75;
   return 7.5625 * t * t + 0.984375;
 }
-
-function bounceEaseOut(gradient: number) {
-  return 1 - bounceEase(1 - gradient);
-}
-
-// https://github.com/BabylonJS/Babylon.js/blob/80c65876dbcdf005c12c37d255a1fe020b36b62b/packages/dev/core/src/Animations/easing.ts
-function bounceEase(gradient: number, bounces = 10, bounciness = 10): number {
-  const y = Math.max(0.0, bounces);
-  if (bounciness <= 1.0) {
-    bounciness = 1.001;
-  }
-  const num9 = Math.pow(bounciness, y);
-  const num5 = 1.0 - bounciness;
-  const num4 = (1.0 - num9) / num5 + num9 * 0.5;
-  const num15 = gradient * num4;
-  const num65 =
-    Math.log(-num15 * (1.0 - bounciness) + 1.0) / Math.log(bounciness);
-  const num3 = Math.floor(num65);
-  const num13 = num3 + 1.0;
-  const num8 = (1.0 - Math.pow(bounciness, num3)) / (num5 * num4);
-  const num12 = (1.0 - Math.pow(bounciness, num13)) / (num5 * num4);
-  const num7 = (num8 + num12) * 0.5;
-  const num6 = gradient - num7;
-  const num2 = num7 - num8;
-  return (
-    (-Math.pow(1.0 / bounciness, y - num3) / (num2 * num2)) *
-    (num6 - num2) *
-    (num6 + num2)
-  );
-}
-
-// function easeOutBounce2(t: number, b: number, c: number, d: number) {
-//   if ((t /= d) < 1 / 2.75) {
-//     return c * (7.5625 * t * t) + b;
-//   } else if (t < 2 / 2.75) {
-//     return c * (7.5625 * (t -= 1.5 / 2.75) * t + 0.75) + b;
-//   } else if (t < 2.5 / 2.75) {
-//     return c * (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375) + b;
-//   } else {
-//     return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
-//   }
-// }
-
-// var start = 0;
-// var end = 300;
-// var frameRate = 60 / 1000; // f/ms
-// var duration = 1000; //ms
-// var currentStep = 0;
-// var newY = 0;
-// var ball = document.querySelector('.ball');
-// function animate() {
-//   currentStep++;
-//   // newY = easeOutElastic(currentStep, start, end - start, frameRate * duration);
-// }
